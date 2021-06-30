e2fa1148420a18bae680768bdbb67576518ae4a4
Jessika-prog <58844655+Jessika-prog@users.noreply.github.com>
2021-06-30 11:29:38 +0200

    premier commit

---
+++ api.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+import uvicorn
+import pandas as pd
+
+
+
+app = FastAPI()
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "ONLINE"}
+
+
+
+@app.get("/test/{key_word}")
+async def test(key_word):
+    return f" le keyword est {key_word}"
+
+
+# uvicorn fichier:objet --reload
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
